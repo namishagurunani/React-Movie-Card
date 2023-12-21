a83d5c167a21523cb368c5ba1097fcce49404da0
shivam gurunani <namisha.gurunani173261@gmail.com>
2023-12-21 23:54:03 +0530

    Movie Project Completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MovieCard from './Components/Card';
+import  Data from './Components/Data.json'
+import "./Components/Card.css"
+function App() {
+  return (
+    <div className="Container">
+    {Data.map((e)=>{
+      return <MovieCard poster={e.poster} moviename={e.moviename} year={e.year} actorName={e.actorName} time={e.time} type={e.type} about={e.about} img={e.img}
+      />
+    })}
+    </div>
+  );
+}
+
+export default App;
